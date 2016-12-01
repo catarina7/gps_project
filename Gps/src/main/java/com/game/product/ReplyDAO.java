8d3c9a6acd8e9316ba70d333c384b00ce2bf3c3c
user1 <user1@KH1>
2016-12-01 13:32:08 +0900

    uupp

--- Gps/src/main/java/com/game/product/ReplyDAO.java
+++ Gps/src/main/java/com/game/product/ReplyDAO.java
@@ -1,8 +1,15 @@
 package com.game.product;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReplyDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private String namespace = "ReplyMapper.";
+	
 	
 }
