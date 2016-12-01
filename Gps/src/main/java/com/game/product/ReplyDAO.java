64cacfb8c3b23ff52434dd0d354a0283535ab8e3
user1 <user1@KH1>
2016-12-01 15:08:00 +0900

    upupup

--- Gps/src/main/java/com/game/product/ReplyDAO.java
+++ Gps/src/main/java/com/game/product/ReplyDAO.java
@@ -1,8 +1,12 @@
 package com.game.product;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.game.util.PageMaker;
 
 @Repository
 public class ReplyDAO {
@@ -11,5 +15,15 @@ public class ReplyDAO {
 	private SqlSession sqlSession;
 	private String namespace = "ReplyMapper.";
 	
+	public int replyWrite(ReplyDTO replyDTO){
+		return sqlSession.insert(namespace+"replyWrite", replyDTO);
+	}
 	
+	public int replyDelete(int num){
+		return sqlSession.delete(namespace+"replyDelete", num);
+	}
+	
+	public List<ReplyDTO> replyList(PageMaker pageMaker){
+		return sqlSession.selectList(namespace+"replyList", pageMaker);
+	}
 }
