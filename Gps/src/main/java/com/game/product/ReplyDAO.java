c4ea216a3c518e6000e380e75e0a867852fcc96f
user1 <user1@KH1>
2016-12-01 15:09:54 +0900

    upupupup

--- Gps/src/main/java/com/game/product/ReplyDAO.java
+++ Gps/src/main/java/com/game/product/ReplyDAO.java
@@ -15,14 +15,15 @@ public class ReplyDAO {
 	private SqlSession sqlSession;
 	private String namespace = "ReplyMapper.";
 	
+	//댓글 작성
 	public int replyWrite(ReplyDTO replyDTO){
 		return sqlSession.insert(namespace+"replyWrite", replyDTO);
 	}
-	
+	//댓글 삭제
 	public int replyDelete(int num){
 		return sqlSession.delete(namespace+"replyDelete", num);
 	}
-	
+	//댓글 리스트 불러오기
 	public List<ReplyDTO> replyList(PageMaker pageMaker){
 		return sqlSession.selectList(namespace+"replyList", pageMaker);
 	}
--- Gps/src/main/java/com/game/product/ReplyService.java
+++ Gps/src/main/java/com/game/product/ReplyService.java
@@ -1,8 +1,13 @@
 package com.game.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReplyService {
+	
+	@Autowired
+	private ReplyDTO replyDTO;
+	
 	
 }
