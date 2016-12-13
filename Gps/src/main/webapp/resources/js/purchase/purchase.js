6a8f319978c2a894ab90f7f306627bd59e50249b
jimmy.an <jimmy.an@ahnfamily>
2016-12-13 14:25:41 +0900

    forbackup

---
+++ Gps/src/main/webapp/resources/js/purchase/purchase.js
@@ -0,0 +1,48 @@
+$(function(){
+	
+	var price_input = 0;
+	
+	price_input = $("#price_input").val();
+	
+	//부가세
+	$("#vat_input").val((price_input*0.1));
+	
+	//마일리지 입력하기
+	$("#millage_count").keyup(function(){
+		if($("#m_millage").val() < $("#millage_count").val()){
+			
+			
+		}else{
+			alert("가용 마일리보다 더 사용 하실수 없습니다.");
+		
+		}
+		
+	});
+	
+	
+	//전부 적용하기
+	$("#mill_in").click(function(){
+		if($("#m_millage").val() != 0){
+			var m_millage = $("#m_millage").val();
+			var millage_count = $("#millage_count").val();
+			$("#millage_count").val(m_millage+millage_count);
+			$("#m_millage").val("");
+		}else{
+			alert("가용 마일리지가 없습니다.");
+		}		
+		
+	});
+	
+	$("#mill_cancle").click(function(){
+		if($("#millage_count").val() != 0){
+			var m_millage = $("#m_millage").val();
+			var millage_count = $("#millage_count").val();
+			$("#m_millage").val(m_millage+millage_count);
+			$("#millage_count").val("");
+		}else{
+			alert("사용한 마일리지 없습니다.");
+		}
+	});
+	
+	
+});
