14463902feecbe06ca32b35e08b209b813889aaf
user1 <user1@KH1>
2016-12-14 13:36:02 +0900

    upupupup

--- Gps/src/main/java/com/game/chat/EchoHandler.java
+++ Gps/src/main/java/com/game/chat/EchoHandler.java
@@ -1,5 +1,8 @@
 package com.game.chat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.CloseStatus;
@@ -11,9 +14,18 @@ public class EchoHandler extends TextWebSocketHandler {
 	
 	private static Logger logger = LoggerFactory.getLogger(EchoHandler.class);
 	
+	private List<WebSocketSession> connectedUsers;
+	
+	public EchoHandler() {
+		// TODO Auto-generated constructor stub
+		connectedUsers = new ArrayList<WebSocketSession>();
+	}
+	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception{
 		logger.info("{0} 연결됨", session.getId());
+		logger.info("연결 IP", session.getRemoteAddress().getHostName());
+		connectedUsers.add(session);
 	}
 	
 	@Override
@@ -21,12 +33,20 @@ public class EchoHandler extends TextWebSocketHandler {
 		// TODO Auto-generated method stub
 		logger.info("{0}로 부터 {1} 받음", session.getId(), message.getPayload());
 		// text 내용 뿌릴때 사용
-		session.sendMessage(new TextMessage("UserID:"+ message.getPayload()));
+		session.sendMessage(new TextMessage(session.getId() +"님이 전송:"+ message.getPayload()));
+		
+		for (WebSocketSession webSocketSession : connectedUsers){
+			//보낸 사용자는 받지 않기 위한 조건문
+			if(!session.getId().equals(webSocketSession)){
+				webSocketSession.sendMessage(new TextMessage(session.getRemoteAddress().getHostName() + "->" + message.getPayload()));
+			}
+		}
 	}
 	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		// TODO Auto-generated method stub
 		logger.info("{0} 연결 끊김", session.getId());
+		connectedUsers.remove(session);
 	}
 }
--- Gps/src/main/webapp/WEB-INF/views/chat/chat.jsp
+++ Gps/src/main/webapp/WEB-INF/views/chat/chat.jsp
@@ -10,24 +10,38 @@
 <script type="text/javascript" src="${pageContext.request.contextPath}/resources/static/js/jquery/sockjs-0.3.4.js"></script>
 <script type="text/javascript">
 	$(document).ready(function(){
+		/* 채팅 버튼 */
 		$("#sendBtn").click(function(){
-			sendMessage();
+			if($("#message").val() != ""){				
+				sendMessage();
+			}else {
+				alert("내용을 입력해주세요!");
+			}
 		});	
+		/* 채팅 끄기 버튼 */
+		$("#closes").click(function(){
+			closeMessage();
+		});
 		
 		var sock;
+		/* 전송 메소드 */
 		function sendMessage() {
 			sock = new SockJS("<c:url value="/echo"/>");
 			sock.onmessage = onMessage;
-			sock.onclose = onClose;
 			sock.onopen = function() {
-				sock.send($("#message").val());
+				sock.send($("#message").val()+"<br/>");
 			};
 		};
+		/* 닫는 메소드 */
+		function closeMessage() {
+			sock.onclose = onClose;
+			sock.close();
+		}
 		
 		function onMessage(evt) {
 			var data = evt.data;
-			$("#data").append(data);
-			sock.close();
+			$("#data").append("나 :" + data + "<br/>");
+			$("message").val("");
 		};
 		
 		function onClose(evt) {
@@ -39,6 +53,7 @@
 <body>
 	<input type="text" id="message">
 	<input type="button" id="sendBtn" value="전송">
+	<input type="button" id="closes" value="나오기">
 	<div id="data"></div>
 </body>
 </html>
