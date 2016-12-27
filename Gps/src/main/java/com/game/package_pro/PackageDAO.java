54bdb8f73fc5a029b0cc969b8f1d31b5d7619925
user1 <user1@KH1>
2016-12-27 13:29:48 +0900

    upupupupup

--- Gps/src/main/java/com/game/package_pro/PackageDAO.java
+++ Gps/src/main/java/com/game/package_pro/PackageDAO.java
@@ -13,22 +13,27 @@ public class PackageDAO {
 	private SqlSession sqlSession;
 	private String namespace = "packageMapper.";
 	
+	//패키지 게임 입력
 	public int packageWrite(PackageDTO packageDTO) throws Exception{
 		return sqlSession.insert(namespace+"packAdd", packageDTO);
 	}
 	
+	//패키지 게임 수정
 	public int packageMod(PackageDTO packageDTO) throws Exception{
 		return sqlSession.update(namespace+"packMod", packageDTO);
 	}
 	
+	//패키지 게임 삭제
 	public int packageDelete(int pack_num) throws Exception{
 		return sqlSession.delete(namespace+"packDelete", pack_num);
 	}
 	
+	//서브 카테고리로 패키지 게임 목록 가져오기
 	public List<PackageDTO> packageList(String sub_category) throws Exception {
 		return sqlSession.selectList(namespace+"packList", sub_category);
 	}
 	
+	//구매 시 패키지 가져오기
 	public List<PackageDTO> packageBuy(int pack_num) throws Exception{
 		return sqlSession.selectList(namespace+"packBuy", pack_num);
 	}
--- Gps/src/main/java/com/game/package_pro/PackageService.java
+++ Gps/src/main/java/com/game/package_pro/PackageService.java
@@ -1,5 +1,7 @@
 package com.game.package_pro;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,31 @@ public class PackageService {
 	
 	@Autowired
 	private PackageDAO packageDAO;
+	
+	//패키지 게임 생성
+	public int packWrite(PackageDTO packageDTO) throws Exception{
+		return packageDAO.packageWrite(packageDTO);
+	}
+	
+	//패키지 수정
+	public int packMod(PackageDTO packageDTO) throws Exception{
+		return packageDAO.packageMod(packageDTO);
+	}
+	
+	//패키지 삭제
+	public int packDelete(int pack_num) throws Exception{
+		return packageDAO.packageDelete(pack_num);
+	}
+	
+	//서브 카테고리로 패키지게임 목록 가져오기
+	public List<PackageDTO> packList(String sub_category) throws Exception{
+		return packageDAO.packageList(sub_category);
+	}
+	
+	//패키지 구매 시 가져오기
+	public List<PackageDTO> packBuy(int pack_num) throws Exception{
+		return packageDAO.packageBuy(pack_num);
+	}
 	
 	
 }
