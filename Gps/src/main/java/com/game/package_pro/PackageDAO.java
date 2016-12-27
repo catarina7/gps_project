f5aef6701615daf9f3a8e74d9dc985b79ab867d9
user1 <user1@KH1>
2016-12-27 13:13:08 +0900

    upupupupupup

--- Gps/src/main/java/com/game/package_pro/PackageDAO.java
+++ Gps/src/main/java/com/game/package_pro/PackageDAO.java
@@ -1,5 +1,7 @@
 package com.game.package_pro;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,26 @@ public class PackageDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	private String namespace = "packageMapper.";
+	
+	public int packageWrite(PackageDTO packageDTO) throws Exception{
+		return sqlSession.insert(namespace+"packAdd", packageDTO);
+	}
+	
+	public int packageMod(PackageDTO packageDTO) throws Exception{
+		return sqlSession.update(namespace+"packMod", packageDTO);
+	}
+	
+	public int packageDelete(int pack_num) throws Exception{
+		return sqlSession.delete(namespace+"packDelete", pack_num);
+	}
+	
+	public List<PackageDTO> packageList(String sub_category) throws Exception {
+		return sqlSession.selectList(namespace+"packList", sub_category);
+	}
+	
+	public List<PackageDTO> packageBuy(int pack_num) throws Exception{
+		return sqlSession.selectList(namespace+"packBuy", pack_num);
+	}
 	
 	
 }
